c45f7a7ff57095b3d5234ea30b86d618c59eee85
stupachenko.artem <stupachenko.a@cadesign.ru>
2025-12-16 22:25:10 +0300

    Деплой контракт

---
+++ backend/script/DeployCrowdfunding.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/Crowdfunding.sol";
+
+contract DeployCrowdfunding is Script {
+    function run() external {
+        vm.startBroadcast();
+        new Crowdfunding(
+            "Crowdfunding Project",
+            "Project description",
+            1 ether
+        );
+        vm.stopBroadcast();
+    }
+}
